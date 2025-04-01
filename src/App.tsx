99c3d563bb92f80cc4cdd23ebf7d7238d86d4ccf
GNcode <gngalarga027@gmail.com>
2025-04-01 12:04:09 -0500

    updating images

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Catalogo de respostería</h1>
+      <img className="w-[500px]" src="/og.jpg" alt="og" />
     </>
   );
 }
