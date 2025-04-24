0eccbb13ebd6e11e11a31a1078c85398339e0a6b
GNcode <gngalarga027@gmail.com>
2025-04-24 07:31:52 -0500

    feat: Implement sharing functionality in Food component and enhance Suscribete form with success/error messages

--- src/components/Principal/Food.tsx
+++ src/components/Principal/Food.tsx
@@ -1,6 +1,20 @@
 import { CatalogoItem } from "../../assets/types/interfaces";
+import { useState } from "react";
 
 export const Food = ({ id, name, urlImg, price }: CatalogoItem) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShare = () => {
+    const text = `En la repostería Dulce despertar ofrecemos ${name}, ven y pruébalo: www.catalogo.com`;
+    navigator.clipboard.writeText(text).then(() => {
+      setShowModal(true);
+      setTimeout(() => setShowModal(false), 2000);
+    }).catch(err=>{
+      alert('Ocurrió un error al copiar');
+      console.error(err);
+    });
+  };
+  
   return (
     <div
       id={name + id}
@@ -13,9 +27,15 @@ export const Food = ({ id, name, urlImg, price }: CatalogoItem) => {
       <h3 className="absolute bg-soft-blue-green rounded-xl px-4 border-2 border-dark-purple bottom-0 left-1/2 translate-y-1/2 -translate-x-1/2 whitespace-nowrap font-bold text-dark-purple">
         $ {price}
       </h3>
-      <button aria-label="Botón para compartir página" className="absolute cursor-pointer top-0 right-0 -translate-y-1/2 translate-x-1/2">
+      <button onClick={handleShare} aria-label="Botón para compartir página" className="absolute cursor-pointer top-0 right-0 -translate-y-1/2 translate-x-1/2">
         <div className='size-7 bg-center bg-cover bg-[url("/images/ui/iconos/compartir.webp")] hover:bg-[url("/images/ui/iconos/compartir_active.webp")] lg:size-10'></div>
       </button>
+
+      {showModal && (
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-soft-blue-green text-dark-purple px-4 py-2 rounded-xl shadow-lg">
+          ¡Texto copiado al portapapeles!
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Suscribete/Suscribete.tsx
+++ src/components/Suscribete/Suscribete.tsx
@@ -1,4 +1,42 @@
+import { useState } from "react";
+
 export const Suscribete = () => {
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    formData.append('access_key', '143f12d9-6167-43c7-be4a-12c65b82899e');
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+        headers: {
+          'Accept': "application/json",
+        },
+      });
+      const data = await response.json();
+
+      if (data.success) {
+        setSuccessMessage("¡Gracias por suscribirte! Hemos recibido tu correo.");
+        setErrorMessage(null);
+        form.reset();
+      } else {
+        setErrorMessage("Hubo un error al enviar el formulario. Inténtalo de nuevo.");
+        setSuccessMessage(null);
+      }
+    } catch (error) {
+      console.error("Error al enviar el formulario:", error);
+      setErrorMessage("Hubo un error al enviar el formulario. Inténtalo de nuevo.");
+      setSuccessMessage(null);
+    }
+  };
+
   return (
     <article className="bg-dark-purple p-4 mx-6 mt-2 rounded-[30px] flex gap-4 text-soft-blue-green mb-6 items-center justify-center sm:mx-10 sm:p-8 sm:rounded-[50px] lg:mx-30 lg:rounded-[64px]">
       <img
@@ -6,12 +44,12 @@ export const Suscribete = () => {
         src="images/ui/banners/banner_suscribete.webp"
         alt="Carta con logo de la empresa"
       />
-      <div className="w-[60%] max-w-[450px]">
+      <div className="relative w-[60%] max-w-[450px]">
         <h2 className="text-2xl font-bold">¡Suscríbete!</h2>
         <p className="text-sm">
           Registra tu correo para recibir nuestras últimas ofertas y eventos
         </p>
-        <form className="mt-2" action="#">
+        <form className="mt-2" onSubmit={handleSubmit}>
           <div className="flex gap-2 bg-soft-blue-green rounded-2xl py-1 px-2">
             <label className="bg-white rounded-xl w-full ">
               <span className="sr-only">Correo electrónico</span>
@@ -21,7 +59,7 @@ export const Suscribete = () => {
                 name="email"
               />
             </label>
-            <button type="submit" className="cursor-pointer">
+            <button type="submit" className="cursor-pointer" disabled={successMessage? true : false}>
               <img
                 className="h-4 w-auto"
                 src="images/ui/iconos/enviar.webp"
@@ -30,6 +68,14 @@ export const Suscribete = () => {
             </button>
           </div>
         </form>
+
+        {successMessage && (
+          <p className="absolute mt-2 text-soft-blue-green text-sm font-bold">{successMessage}</p>
+        )}
+
+        {errorMessage && (
+          <p className="absolute mt-2 text-intense-rose text-sm font-bold">{errorMessage}</p>
+        )}
       </div>
     </article>
   );
